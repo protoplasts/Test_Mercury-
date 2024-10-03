f3db9f3374ee18ce8d370c64dfdf4289b9d3278a
景泓 堯 <dd1115dd1115@gmail.com>
2024-10-03 16:57:39 +0800

    .NET Core 8 Web API

---
+++ WebAPI/AppDbContext .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI; 
+namespace WebAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
